296f283d2d5c67719e08861f4caf3883200ff45c
seondal <sseondal@gmail.com>
2023-10-13 10:44:03 +0900

    🛠 fix :  네비게이션 바 이슈

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,42 +1,19 @@
 import React, { useState } from "react";
 
 export const AppBar: React.FC = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <div>
-      <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
-        <div className="navbar-end">
+      <div className="navbar flex h-20 flex-row justify-between px-20 md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
+        <h1 className="text-4xl">Ploverse</h1>
+        <div className="">
           <label
             htmlFor="my-drawer"
-            className="btn-gh items-center justify-between md:hidden mr-5"
-            onClick={() => setIsNavOpen(!isNavOpen)}>
+            className="btn-gh items-center justify-between mr-5">
             <div className="HAMBURGER-ICON space-y-2.5 ml-5">
-              <div
-                className={`h-0.5 w-8 bg-purple-600 ${
-                  isNavOpen ? "hidden" : ""
-                }`}
-              />
-              <div
-                className={`h-0.5 w-8 bg-purple-600 ${
-                  isNavOpen ? "hidden" : ""
-                }`}
-              />
-              <div
-                className={`h-0.5 w-8 bg-purple-600 ${
-                  isNavOpen ? "hidden" : ""
-                }`}
-              />
+              <div className={`h-0.5 w-8 bg-purple-600 `} />
+              <div className={`h-0.5 w-8 bg-purple-600 `} />
+              <div className={`h-0.5 w-8 bg-purple-600 `} />
             </div>
-            <div
-              className={`absolute block h-0.5 w-8 animate-pulse bg-purple-600 ${
-                isNavOpen ? "" : "hidden"
-              }`}
-              style={{ transform: "rotate(45deg)" }}></div>
-            <div
-              className={`absolute block h-0.5 w-8 animate-pulse bg-purple-600 ${
-                isNavOpen ? "" : "hidden"
-              }`}
-              style={{ transform: "rotate(135deg)" }}></div>
           </label>
         </div>
       </div>
--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,5 +1,3 @@
-import { FC } from "react";
-import Link from "next/link";
 import Text from "./Text";
 import NavElement from "./nav-element";
 interface Props {
@@ -10,11 +8,10 @@ export const ContentContainer: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex-1 drawer h-52">
       <input id="my-drawer" type="checkbox" className="grow drawer-toggle" />
-      <div className="items-center  drawer-content">{children}</div>
+      <div className="items-center drawer-content">{children}</div>
       {/* SideBar / Drawer */}
       <div className="drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay gap-6"></label>
-
         <ul className="p-4 overflow-y-auto menu w-80 bg-base-100 gap-10 sm:flex items-center">
           <li>
             <Text
