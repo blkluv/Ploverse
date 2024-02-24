e7cec4cdd170db228a4aa9d11c8f146d8a010edf
The Wizard of Hahz <info@blkluv.org>
2024-02-24 09:38:41 -0500

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,16 @@ const Home: NextPage = (props) => {
     <div>
       <div className="flex flex-col justify-center items-center gap-8 p-8">
         <h1 className="text-7xl">
-          jo<span className="text-accent">GIG</span> <span className="text-accent">Eco</span>nomy
+          jo<span className="text-accent">gig</span> <span className="text-accent">Eco</span>nomy
         </h1>
         <h3 className="text-xs">
-          Get paid for jogging and cleaning your city in the Jogig economy.
+          Get paid for jogiging and cleaning your city in the new Jogig <span className="text-accent">Eco</span>nomy.🏃‍♂️💰🏙️
         </h3>
         <Image src={"/logo.png"} alt="logo" width={150} height={150} />
         <div className="flex flex-col gap-4 text-center max-w-[1200px]">
           <div>
-            <b className="text-accent">jogig</b> the reVULutionary eco-friendly activity get paid for jogging and cleaning your city! 🏃‍♂️💰🏙️<br />
-            <b>jog</b>, pick up litter, and earn digital tokens that can be redeemed for real-world rewards. 🌱🏃‍♂️💎<br />
-            <b>Join</b> the <b className="text-accent">jogig</b> community and become a part of the movement to create a cleaner and healthier planet. 🌍💚<br />
+            Go <b>Jogiging</b>with a bag, pick up litter, and earn digital currency that can be redeemed for real-world rewards. 🌱🏃‍♂️💎<br />
+            <b>Join</b> the <b className="text-accent">jogig</b> LUV NFT Discord biz community and become a part of the movement to create a cleaner and healthier planet. 🌍💚<br />
             <b>Elevate</b> your jogging routine and make a positive impact on your city with joGIG one step at a time! 👟🏙️
           </div>
         </div>
