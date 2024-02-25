49aa8b8037e9bab506dbe65e14c003896e957394
The Wizard of Hahz <info@blkluv.org>
2024-02-24 23:58:42 -0500

    solana

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,17 @@ const Home: NextPage = (props) => {
     <div>
       <div className="flex flex-col justify-center items-center gap-8 p-8">
         <h1 className="text-7xl">
-          <center>jo<span className="text-accent">GIG</span></center><br />
-          <center><span className="text-accent">ECO</span>nomy</center>
+          <center>jo<span className="text-accent">GIG</span></center>
         </h1>
         <h3 className="text-xs">
-          Get paid for jogiging and cleaning your city in the new Jogig <span className="text-accent">Eco</span>nomy.🏃‍♂️💰🏙️
+          Get paid for jogiging and cleaning your city in the new Solana blockchain jo<span className="text-accent"></span>GIG</span> <span className="text-accent">ECO</span>nomy.🏃‍♂️💰🏙️
         </h3>
         <Image src={"/logo.png"} alt="logo" width={150} height={150} />
         <div className="flex flex-col gap-4 text-center max-w-[1200px]">
           <div>
             Go <b>Jogiging</b> with a bag, pick up litter, and earn digital currency that can be redeemed for real-world rewards. 🌱🏃‍♂️💎<br />
             <b>Join</b> the <b className="text-accent">jogig</b> LUV NFT Discord biz community and become a part of the movement to create a cleaner and healthier planet. 🌍💚<br />
-            <b>Elevate</b> your jogging routine and make a positive impact on your city with joGIG one step at a time! 👟🏙️
+             
           </div>
         </div>
         <Link href={"/create"}>
